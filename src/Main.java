bf5ee2cb9b141b83e78f2e2cce5c68a0d7be9542
tokslaw-shruboak <147814066+tokslaw-shruboak@users.noreply.github.com>
2024-12-14 10:37:00 -0500

    Using Method to calc area of triangle . Invoking the method
    Signed-off-by: tokslaw-shruboak <147814066+tokslaw-shruboak@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,27 @@
 public class Main {
-    public static void main(String[] args) {
-        double triangleBase = 3.5;
-        double triangleHeight = 7.0;
 
+    public static void main(String[] args) {
+
+    /*
+    The formula for a triangle is (base * height) / 2
+    So lets define 2 variables for the needed values and do the calculation below
+    */
+
+        double area = calculateArea(3.5, 7.0);
+        System.out.println("The area of the triangle 1 is " + area);
+
+        area = calculateArea(4.0, 10.0);
+        System.out.println("The area of the triangle 2 is " + area);
+
+        area = calculateArea(12.0, 3.0);
+        System.out.println("The area of the triangle 3 is " + area);
+
+    }
+
+    public static double calculateArea(double triangleBase, double triangleHeight){
         double area = triangleBase * triangleHeight;
         area = area / 2;
 
-        //triangle 2
-        double triangleBase2 = 4.0;
-        double triangleHeight2 = 10.0;
-
-        double area2 = triangleBase2 * triangleHeight2 ;
-        area2 = area2 / 2;
-
-        //triangle 3
-        double triangleBase3 = 12.0;
-        double triangleHeight3 = 3.0;
-
-        double area3 = triangleBase3 * triangleHeight3;
-        area3 = area3 / 2;
-
-        System.out.println("The area of the triangle is " + area);
-        System.out.println("The Area of Triangle 2: " + area2);
-        System.out.println("Area of Triangle 3: " + area3);
+        return area;
     }
 }
