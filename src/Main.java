95b5ce18121a3a751ce74fed84847c927cb03623
tokslaw-shruboak <147814066+tokslaw-shruboak@users.noreply.github.com>
2024-12-14 10:29:50 -0500

    Calc area of 3 triangles

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,22 @@ public class Main {
         double area = triangleBase * triangleHeight;
         area = area / 2;
 
+        //triangle 2
+        double triangleBase2 = 4.0;
+        double triangleHeight2 = 10.0;
+
+        double area2 = triangleBase2 * triangleHeight2 ;
+        area2 = area2 / 2;
+
+        //triangle 3
+        double triangleBase3 = 12.0;
+        double triangleHeight3 = 3.0;
+
+        double area3 = triangleBase3 * triangleHeight3;
+        area3 = area3 / 2;
+
         System.out.println("The area of the triangle is " + area);
+        System.out.println("Area of Triangle 2: " + area2);
+        System.out.println("Area of Triangle 3: " + area3);
     }
 }
