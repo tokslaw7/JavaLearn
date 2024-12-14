9daa9113f10faa55e7248f62af27f9a907685afa
tokslaw-shruboak <147814066+tokslaw-shruboak@users.noreply.github.com>
2024-12-14 10:21:39 -0500

    Calc area of triangle (1)

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        double triangleBase = 3.5;
+        double triangleHeight = 7.0;
+
+        double area = triangleBase * triangleHeight;
+        area = area / 2;
+
+        System.out.println("The area of the triangle is " + area);
+    }
+}
